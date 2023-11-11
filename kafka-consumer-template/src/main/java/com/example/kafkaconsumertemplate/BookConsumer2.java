343194d4d9028063dc20609b6f13125f1a622b52
mayankRepo01 <83690350+mayankRepo01@users.noreply.github.com>
2023-11-11 20:04:16 +0530

    Update BookConsumer2.java
    Updated to support json deserialization

--- kafka-consumer-template/src/main/java/com/example/kafkaconsumertemplate/BookConsumer2.java
+++ kafka-consumer-template/src/main/java/com/example/kafkaconsumertemplate/BookConsumer2.java
@@ -15,12 +15,11 @@ import java.util.concurrent.TimeUnit;
 public class BookConsumer2 {
     Logger logger= LoggerFactory.getLogger(BookConsumer2.class);
     @KafkaListener(topics = "${kafka.topic.json}",groupId = "test2")
-    public void consumeMessage(String book) throws JsonProcessingException, InterruptedException {
+    public void consumeMessage(Book book) throws JsonProcessingException, InterruptedException {
         long time=ThreadLocalRandom.current().nextLong(500,2000);
         TimeUnit.MILLISECONDS.sleep(time);
-        ObjectMapper objectMapper=new ObjectMapper();
-        Book book1=objectMapper.readValue(book,Book.class);
         logger.info("Consume Book Payload by 2:- "+book);
 
     }
 }
+
